f6a3b8eee04311fc9bea642495cc59bc7374d4d6
Marcio Telis <marciotelis@garten.com.br>
2020-03-27 19:46:05 -0300

    tornando a lista com _ dinâmica com for

--- Jogos/forca.py
+++ Jogos/forca.py
@@ -3,8 +3,11 @@ def jogar():
     print("   Bem vindo ao jogo da forca!")
     print("**********************************")
 
-    palavra_secreta = "banana".upper()
-    letras_acertadas = ["_", "_", "_", "_", "_", "_"]
+    palavra_secreta = "abacate".upper()
+    letras_acertadas = []
+
+    for letra in palavra_secreta:
+        letras_acertadas.append("_")
 
     enforcou = False
     acertou = False

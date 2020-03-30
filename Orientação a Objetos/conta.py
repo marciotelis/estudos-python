c07ee67cadac77c069e91f0ced378b06fb86d042
Marcio Telis <marciotelis@garten.com.br>
2020-03-30 10:55:49 -0300

    encapsulando atributos (tornando-os privados)

--- Orientação a Objetos/conta.py
+++ Orientação a Objetos/conta.py
@@ -2,16 +2,16 @@ class Conta:
     
     def __init__(self, numero, titular, saldo, limite): #Função construtora, para construir o objeto (self é a referência)
         print("construindo objeto ... {}".format(self))
-        self.numero = numero
-        self.titular = titular
-        self.saldo = saldo
-        self.limite = limite
+        self.__numero = numero  #__transforma o atributo numero privado, para não poder acessá-lo diretamente e sim através dos métodos apenas
+        self.__titular = titular
+        self.__saldo = saldo
+        self.__limite = limite
         
     def extrato(self):
-        print("O Saldo do titular {} é {}".format(self.titular, self.saldo))
+        print("O Saldo do titular {} é {}".format(self.__titular, self.__saldo))
         
     def deposita(self, valor):
-        self.saldo += valor
+        self.__saldo += valor
     
     def saca(self, valor):
-        self.saldo -= valor
+        self.__saldo -= valor

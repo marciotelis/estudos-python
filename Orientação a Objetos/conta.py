330b06c18b0fd0be39ad17d4bb84fbe76080c7d4
Marcio Telis <marciotelis@garten.com.br>
2020-03-30 12:07:38 -0300

    Criando getters e setters

--- Orientação a Objetos/conta.py
+++ Orientação a Objetos/conta.py
@@ -19,3 +19,20 @@ class Conta:
     def transfere(self, valor, destino):
         self.saca(valor)
         destino.deposita(valor)
+
+    @property
+    def saldo(self):
+        return self.__saldo
+    
+    @property
+    def titular(self):
+        return self.__titular
+    
+    @property
+    def limite(self):
+        print(self.__limite)
+        return self.__limite
+
+    @limite.setter
+    def limite(self, limite):
+        self.__limite = limite

f4b1bc82ec665e368a31af9771087bc2d163dce0
Marcio Telis <marciotelis@garten.com.br>
2020-03-30 10:01:26 -0300

    criando métodos

--- Orientação a Objetos/conta.py
+++ Orientação a Objetos/conta.py
@@ -1,5 +1,17 @@
 class Conta:
     
-    def __init__(self): #Função construtora, para construir o objeto
-        print("Costruindo obejto... {}".format(self))
+    def __init__(self, numero, titular, saldo, limite): #Função construtora, para construir o objeto (self é a referência)
+        print("construindo objeto ... {}".format(self))
+        self.numero = numero
+        self.titular = titular
+        self.saldo = saldo
+        self.limite = limite
         
+    def extrato(self):
+        print("O Saldo do titular {} é {}".format(self.titular, self.saldo))
+        
+    def deposita(self, valor):
+        self.saldo += valor
+    
+    def saca(self, valor):
+        self.saldo -= valor

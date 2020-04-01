2e7f93c0d39454a2b6b82e2c032140ea01c41ad9
Marcio Telis <marciotelis@garten.com.br>
2020-04-01 17:06:00 -0300

    métodos abstratos

--- Collections/herancaEPolimorfismo.py
+++ Collections/herancaEPolimorfismo.py
@@ -1,3 +1,5 @@
+from abc import ABCMeta, abstractmethod
+
 class Conta:
     def __init__(self, codigo):
         self.codigo = codigo
@@ -8,6 +10,10 @@ class Conta:
 
     def __str__(self):
         return f"[>>Código {self.codigo} Saldo {self._saldo}<<]"
+
+    @abstractmethod     # método abstrato, para criar uma classe filha deve ter este método na classe filha, senao dará erro na hora de instanciar a classe filha
+    def passa_o_mes(self):
+        pass
 
 
 class ContaCorrente(Conta):
